b77de7a59ae80de8d2b8faae7e8b5887fc1652a9
zorque2009 <78483295+zorque2009@users.noreply.github.com>
2021-04-07 15:49:27 +0100

    hard coded pathes removed

--- waterways/NorwayNorth/NorwayNorth.bat
+++ waterways/NorwayNorth/NorwayNorth.bat
@@ -1,32 +1,38 @@
-REM 20210311
-REM ------------------- REPLACE <YOUR FOLDER> WINDOWS STYLE (WITH \ AS SEPARATOR) -------------------
+REM 20210402
 
-cd <YOUR FOLDER>\waterways\tmp
+cd..
+cd tmp
 del * /Q
 
-copy /y %~dp0"rivers - stats.html"
+copy /y %~dp0"rivers-stats.html"
+
+cd..
 
 REM -------------------                   BEGINNING PREPARE.TXT                   ------------------- 
 REM -------------------           TO CROP AN AREA ADD TO THE .o5m LINE            -------------------
 REM -------------------     -b=<WEST>,<SOUTH>,<EAST>,<NORTH> --complete-ways      -------------------
 
-<YOUR FOLDER>\waterways\tools\wget http://download.geofabrik.de/europe/norway-latest.osm.pbf
-<YOUR FOLDER>\waterways\tools\osmconvert64-0.8.8p norway-latest.osm.pbf -b=3,66,34,85 --complete-ways -o=norway-latest.o5m
+tools\wget -P tmp http://download.geofabrik.de/europe/norway-latest.osm.pbf
+tools\osmconvert64-0.8.8p tmp/norway-latest.osm.pbf -b=3,66,34,85 --complete-ways -o=tmp/norway.o5m
 
-<YOUR FOLDER>\waterways\tools\wget http://download.geofabrik.de/europe/finland-latest.osm.pbf
-<YOUR FOLDER>\waterways\tools\osmconvert64-0.8.8p finland-latest.osm.pbf -o=finland-latest.o5m
+tools\wget -P tmp http://download.geofabrik.de/europe/finland-latest.osm.pbf
+tools\osmconvert64-0.8.8p tmp/finland-latest.osm.pbf -o=tmp/finland.o5m
 
-<YOUR FOLDER>\waterways\tools\osmconvert64-0.8.8p.exe norway-latest.o5m finland-latest.o5m -o=rivers.o5m
+tools\osmconvert64-0.8.8p.exe tmp/norway.o5m tmp/finland.o5m -o=tmp/rivers.o5m
 
 REM -------------------                END PREPARE.TXT                  ------------------- 
 
-<YOUR FOLDER>\waterways\tools\osmfilter.exe rivers.o5m --keep= --keep-ways="natural=coastline waterway= ( man_made=pipeline and type=water ) ( man_made=pipeline and substance=water )" --drop="waterway=fairway waterway=lock_gate waterway=riverbank waterway=dam" >waterways.osm
+tools\osmfilter.exe tmp\rivers.o5m --keep= --keep-ways="natural=coastline waterway= ( man_made=pipeline and type=water ) ( man_made=pipeline and substance=water )" --drop="waterway=fairway waterway=lock_gate waterway=riverbank waterway=dam" >tmp/waterways.osm
 
-<YOUR FOLDER>\waterways\tools\solve.py
 
-copy /y "rivers - stats.html" %~dp0"rivers - stats.html"
+
+tools\solve.py
+
+cd tmp
+copy /y "rivers - stats.html" %~dp0"rivers-stats.html"
 copy /y "rivers.html" %~dp0"rivers.html"
 
-<YOUR FOLDER>\waterways\tools\Maperitive.exe
+cd..
+tools\Maperitive\Maperitive.exe
 
 pause
