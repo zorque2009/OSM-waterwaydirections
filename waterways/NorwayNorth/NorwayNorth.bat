af494a5e9c2a8cd6261f008b88b9c1df41d43f3e
zorque2009 <78483295+zorque2009@users.noreply.github.com>
2021-04-10 10:08:51 +0100

    creation of download list automated

--- waterways/NorwayNorth/NorwayNorth.bat
+++ waterways/NorwayNorth/NorwayNorth.bat
@@ -1,35 +1,24 @@
-REM 20210402
+REM 20210409
 
 cd..
 cd tmp
 del * /Q
 
 copy /y %~dp0"rivers-stats.html"
+copy /y %~dp0"urls.txt"
 
 cd..
 
-REM -------------------                   BEGINNING PREPARE.TXT                   ------------------- 
-REM -------------------           TO CROP AN AREA ADD TO THE .o5m LINE            -------------------
-REM -------------------     -b=<WEST>,<SOUTH>,<EAST>,<NORTH> --complete-ways      -------------------
+tools\prepare.py
 
-tools\wget -P tmp http://download.geofabrik.de/europe/norway-latest.osm.pbf
-tools\osmconvert64-0.8.8p tmp/norway-latest.osm.pbf -b=3,66,34,85 --complete-ways -o=tmp/norway.o5m
-
-tools\wget -P tmp http://download.geofabrik.de/europe/finland-latest.osm.pbf
-tools\osmconvert64-0.8.8p tmp/finland-latest.osm.pbf -o=tmp/finland.o5m
-
-tools\osmconvert64-0.8.8p.exe tmp/norway.o5m tmp/finland.o5m -o=tmp/rivers.o5m
-
-REM -------------------                END PREPARE.TXT                  ------------------- 
+call tmp\prepare.bat
 
 tools\osmfilter.exe tmp\rivers.o5m --keep= --keep-ways="natural=coastline waterway= ( man_made=pipeline and type=water ) ( man_made=pipeline and substance=water )" --drop="waterway=fairway waterway=lock_gate waterway=riverbank waterway=dam" >tmp/waterways.osm
-
-
 
 tools\solve.py
 
 cd tmp
-copy /y "rivers-stats.html" %~dp0"rivers-stats.html"
+copy /y "rivers - stats.html" %~dp0"rivers-stats.html"
 copy /y "rivers.html" %~dp0"rivers.html"
 
 cd..
