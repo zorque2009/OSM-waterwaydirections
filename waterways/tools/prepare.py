3b6876ce172555753bab6bc8f1266c5971d50327
zorque2009 <78483295+zorque2009@users.noreply.github.com>
2021-04-10 10:07:41 +0100

    creation of download list automated

--- waterways/tools/prepare.py
+++ waterways/tools/prepare.py
@@ -1,7 +1,5 @@
 import os
 
-os.chdir("..")
-
 urls = open("tmp/urls.txt", "r")                  #Load urls
 urls = urls.read().splitlines()
 
@@ -9,17 +7,28 @@ text = ""
 unite = ""
 for x in urls:
 
-    line1 = "tools\wget -P tmp " + x
+    x = x.rsplit(" ",1)
+    
+    crop = " "
+    if len(x) > 1:
+        crop = " -b=" + x[1] + " --complete-ways "
+        # -b=-40,10,80,50 --complete-ways 
+
+    url=x[0]
+    
+
+    
+    line1 = "tools\wget -P tmp " + url
  
-    x = x.rsplit("/",1)
-    infile = x[1] 
-    x = x[1].rsplit("-",1)
-    x = x[0]
-    outfile = "tmp/" + x + ".o5m"                           #merge regions in osmconvert
+    url = url.rsplit("/",1)
+    infile = url[1] 
+    url = url[1].rsplit("-",1)
+    url = url[0]
+    outfile = "tmp/" + url + ".o5m"                           #merge regions in osmconvert
     unite = unite + outfile + " "
     outfile = "-o=" + outfile 
     
-    line2 = "tools\osmconvert64-0.8.8p tmp/" + infile + " " + outfile
+    line2 = "tools\osmconvert64-0.8.8p tmp/" + infile + crop + outfile
   
     text = text + (line1) + "\n"
     text = text + (line2) + "\n" + "\n"
@@ -28,10 +37,5 @@ unite = ("tools\osmconvert64-0.8.8p.exe " + unite)
 text = text + unite + "-o=tmp/rivers.o5m"
 
 print text
-with open(r'tmp/prepare.txt', "w") as prepare:
-    prepare.write((text))
-    
-
-nothing = raw_input('done')
-
-    
+with open(r'tmp/prepare.bat', "w") as prepare:
+    prepare.write((text))   
